6e49a955ecdaaac5787b39249af463d0fa2b579b
Finley <finleyrobbins@Finleys-MacBook-Air.local>
2025-06-17 00:25:59 -0600

    final

--- lmexampleR.R
+++ lmexampleR.R
@@ -6,3 +6,8 @@ fit = lm(y ~ x)
 summary(fit)
 plot(x,y)
 abline(fit, col=2)
+getwd()
+co2 = read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
